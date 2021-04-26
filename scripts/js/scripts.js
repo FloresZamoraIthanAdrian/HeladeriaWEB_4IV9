b0769c745cfa999f51d33bac6b2a9773d44b38ad
FloresZamoraIthanAdrian <ifloresz1900@alumno.ipn.mx>
2021-04-25 22:07:46 -0500

    Validacion de formularios

--- scripts/js/scripts.js
+++ scripts/js/scripts.js
@@ -13,3 +13,39 @@ jQuery('document').ready(function($){
         }
     });
 });
+
+function validarLetras(e){
+    var teclado = (document.all) ? e.keyCode : e.which;
+
+    if(teclado == 8) return true;
+
+    var patron = /[a-z-A-Z]/;
+
+    var prueba = String.fromCharCode(teclado);
+
+    return patron.test(prueba); 
+}
+
+function validarAlfaNumericos(e){
+    var teclado = (document.all) ? e.keyCode : e.which;
+
+    if(teclado == 8) return true;
+
+    var patron = /[a-z-A-Z\d]/;
+
+    var prueba = String.fromCharCode(teclado);
+
+    return patron.test(prueba);
+}
+
+function validarNumeros(e){
+    var teclado = (document.all) ? e.keyCode : e.which;
+
+    if(teclado == 8) return true;
+
+    var patron = /\d/;
+
+    var prueba = String.fromCharCode(teclado);
+
+    return patron.test(prueba);
+}
